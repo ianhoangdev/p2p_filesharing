43fcca84223e261f687e1857cc08b695f64626a9
Ian Hoang <134716435+ianhoangdev@users.noreply.github.com>
2025-10-19 10:16:28 -0400

    getting neighbors for peers

--- peerProcess.py
+++ peerProcess.py
@@ -34,3 +34,29 @@ class Peer:
         
         self.bitfield = Bitfield(self.file_size, self.piece_size, self.has_file)
         self.total_pieces = self.bitfield.total_pieces
+
+        logger.info(f"File: {self.common_cfg['FileName']}, Size: {self.file_size} bytes")
+        logger.info(f"Total pieces: {self.total_pieces}, Piece size: {self.piece_size} bytes")
+        if self.has_file:
+            logger.info("I am a 'seed' (I have the complete file).")
+
+        self.neighbors = {}
+        for p_info in self.peer_info_list:
+            if p_info['peer_id'] != self.peer_id:
+                self.neighbors[p_info['peer_id']] = {
+                    'host': p_info['host_name'],
+                    'port': p_info['port'],
+                    'has_file_initially': p_info['has_file'],
+                    'bitfield': Bitfield(self.file_size, self.piece_size, p_info['has_file']),
+                    'am_choking': True,       # am I choking this neighbor?
+                    'is_choking_me': True,    # is this neighbor choking me?
+                    'me_interested': False,   # am I interested in their pieces?
+                    'them_interested': False,   # are they interested in my pieces?
+                    'socket': None,           # will hold the socket object
+                    'handler_thread': None    # will hold the connection thread
+                }
+        
+        logger.info(f"Initialized state for {len(self.neighbors)} neighbors.")
+
+    def run(self):
+        logger.info("Peer run() method called.")
